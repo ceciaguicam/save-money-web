067047f41a2510578b3439843c33034d688483cc
Ceci Aguilera <ceci.aguicam@gmail.com>
2022-12-03 20:30:25 +0100

    Se pintan las variables gasto, ahorro e ingreso en pantalla cuando se inicia la app

--- index.js
+++ index.js
@@ -16,3 +16,35 @@ Necesitamos desarrollar una web que nos ayude a gestionar nuestros ingresos y ga
 ![Gastos introducidos](./example2.JPG)
 */
 
+const movementList = document.querySelector(".movements") //Identifico el sitio donde pintaré los movimientos
+
+let spent = 0 //Sumatorio de los gastos
+let income = 0 //Sumatorio de los ingresos
+let save = 0 //Diferencia entre los ingresos y los gastos
+
+let saveAmount = document.querySelector(".save") //Identifico el sitio donde se actualizará el ahorro
+let incomeAmount = document.querySelector(".income") //Identifico el sito donde se actualizará el total de ingresos
+let spentAmount = document.querySelector(".spent") //Identifico el sito donde se actualizará el total de gastos
+
+saveAmount.innerHTML = `${save} €`
+incomeAmount.innerHTML = `${income} €`
+spentAmount.innerHTML = `${spent} €`
+
+
+
+function addAMovement(){
+
+    let movementName = document.getElementById("movement-name").value //Consigo el valor del input donde se escribe el nombre del movimiento
+    let movementAmount = document.getElementById("movement-amount").value
+
+    let movementContent = `<p>${movementName}: ${movementAmount} €</p>` //Añado el nombre del movimiento al contenido del movimiento
+
+    const movement = document.createElement("article") //Creo un article nuevo
+
+    movement.innerHTML = movementContent //Añado el contenido del movimiento al article creado
+    movementList.prepend(movement) //Añado el article a la sección de movimientos
+}
+
+function anotherSpent(amountSpent){
+
+}
